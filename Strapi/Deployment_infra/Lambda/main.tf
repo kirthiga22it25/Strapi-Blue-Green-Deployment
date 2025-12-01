88bcfadf19fd5bf0a0336e6279d799be27f5a194
kirthiga22it25 <kirthiga.balakrrishnan@gmail.com>
2025-12-01 15:55:39 +0530

    Updated S3 Bucket name

--- Strapi/Deployment_infra/Lambda/main.tf
+++ Strapi/Deployment_infra/Lambda/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "ecs_cleanup" {
   function_name = "ecs-cleanup"
-  s3_bucket     = "lambda-script-strapi-cleanup"
+  s3_bucket     = "strapi-codedeploy-kirthiga-01"
   s3_key        = "ecs_cleanup.zip"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
